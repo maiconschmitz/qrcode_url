94ef72321344f633dbe735761dcf410ca7b82403
Maicon Schmitz <maiconschmitz@gmail.com>
2017-07-07 14:49:20 -0300

    Efetuada a liberação dos fontes da extensão.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# qrcode_url
+## QRCode URL
 Extensão do Google Chrome para gerar um QRCode a partir da URL atualmente aberta
+
+## Informações sobre a extensão: http://www.maiconschmitz.com.br/blog/2017/03/30/gerando-qr-code-de-url-no-google-chrome/
+
+## Extensão publicada na Chrome Web Store
+Caso seja de seu interesse, você pode efetuar o [Download da Extensão QRCode URL](https://goo.gl/jLk9hk) para o seu Google Chrome.
---
+++ js/extensao.js
@@ -0,0 +1,47 @@
+(function(window,document)
+{ 
+    chrome.tabs.getSelected(null, function(tab) {
+        criarQRCode(tab.url);
+    });
+
+    function criarQRCode(conteudo) 
+    {
+        jQuery('#qrcode').qrcode({
+            text : conteudo
+        });
+
+        $('#loader').hide(200, function() { $('#qrcode').show(300); } );
+    }
+    
+    window.onload = function()
+    {
+        var opts = {
+              lines: 13             // The number of lines to draw
+            , length: 7             // The length of each line
+            , width: 4              // The line thickness
+            , radius: 10            // The radius of the inner circle
+            , scale: 1              // Scales overall size of the spinner
+            , corners: 1            // Corner roundness (0..1)
+            , color: '#000'         // #rgb or #rrggbb or array of colors
+            , opacity: 0.25         // Opacity of the lines
+            , rotate: 0             // The rotation offset
+            , direction: 1          // 1: clockwise, -1: counterclockwise
+            , speed: 1              // Rounds per second
+            , trail: 60             // Afterglow percentage
+            , fps: 20               // Frames per second when using setTimeout() as a fallback for CSS
+            , zIndex: 2e9           // The z-index (defaults to 2000000000)
+            , className: 'spinner'  // The CSS class to assign to the spinner
+            , top: '50%'            // Top position relative to parent
+            , left: '50%'           // Left position relative to parent
+            , shadow: false         // Whether to render a shadow
+            , hwaccel: false        // Whether to use hardware acceleration
+            , position: 'absolute'  // Element positioning
+        }
+        
+        var target = document.getElementById('loader');
+        var spinner = new Spinner(opts).spin(target);
+        
+        $('#loader').show();
+        $('#qrcode').hide();
+    }
+})(window,document);
